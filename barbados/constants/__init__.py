2ad5e4da38e419bb12d77f0392389d843ef2078d
Grant Cohoe <grant@grantcohoe.com>
2020-02-08 14:54:50 -0500

    validation of ingredients, different types

--- barbados/constants/__init__.py
+++ barbados/constants/__init__.py
@@ -5,5 +5,5 @@ class IngredientTypes(enum.Enum):
     CATEGORY = 'category'
     FAMILY = 'family'
     INGREDIENT = 'ingredient'
-    ALIAS = 'alias'
     PRODUCT = 'product'
+    CUSTOM = 'custom'
