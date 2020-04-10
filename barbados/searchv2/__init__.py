92c235a91134a0a4db6dc0c631a7b5ca386ca75b
Grant Cohoe <grant@grantcohoe.com>
2020-04-09 20:23:28 -0400

    simply init

--- barbados/searchv2/__init__.py
+++ barbados/searchv2/__init__.py
@@ -48,43 +48,96 @@ class CocktailSearchResults(SearchResults):
 
 
 class BaseSearch:
-    def __init__(self):
-        self.q = None
-        self.sort = None
-        self.query_parameters = {}
+    """
+    Generic class to manage querying an ElasticSearch index. This class is wicked yuuuge
+    so that the specific index implementations don't have to be.
+    """
 
+    def __init__(self, **kwargs):
+        self.query_parameters = {}
         self._build_query_parameters()
+
+        musts = []
+
+        for key, value in kwargs.items():
+            if key not in self.supported_parameters:
+                raise KeyError("Query parameter %s is not supported" % key)
+            setattr(self, key, value)
+
+        for parameter in self.supported_parameters:
+            values = getattr(self, parameter, None)
+            if not values:
+                continue
+
+            # We split in the URL based on ,'s.
+            values = values.split(',')
+
+            for value in values:
+                if not value:
+                    continue
+                musts.append(self.get_query_condition(parameter=parameter, value=value))
+
+        logging.info("Search Conditions are %s" % musts)
+        self.q = Bool(must=musts)
 
     @property
     def index_class(self):
+        """
+        Define the class of the ElasticSearch index for this search.
+        :return:
+        """
         raise NotImplementedError
 
     @property
     def result_class(self):
+        """
+        Define the class that represents results of this search.
+        :return:
+        """
         raise NotImplementedError
 
     def _build_query_parameters(self):
+        """
+        Perform a series of self.add_query_parameter() to define what the parameters
+        are that this class supports.
+        :return:
+        """
         raise NotImplementedError
 
-    def execute(self):
+    def execute(self, sort='_score'):
         """
         Actually talk to ElasticSearch and run the query.
+        :param sort: ElasticSearch attribute on which to sort the results.
         :return: SearchResults child class.
         """
-        results = self.index_class.search()[0:1000].query(self.q).sort(self.sort).execute()
+        results = self.index_class.search()[0:1000].query(self.q).sort(sort).execute()
         logging.info("Got %s results." % results.hits.total.value)
         return self.result_class(hits=results)
 
     def add_query_parameter(self, parameter, query_class, query_key, **attributes):
+        """
+        Define a queriable parameter for this search index.
+        :param parameter: URL/input parameter to key from.
+        :param query_class: ElasticSearch DSL query class.
+        :param query_key: ElasticSearch query key field (usually not the same as an input parameter)
+        :param attributes: Dictionary of extra params to pass to the query_class consturctor
+        :return: None
+        """
         self.query_parameters[parameter] = {
             'parameter': parameter,
             'query_class': query_class,
             'query_key': query_key,
             'attributes': attributes
         }
-        logging.info("%s: %s" % (parameter, self.query_parameters[parameter]))
 
-    def get_query_object(self, parameter, value):
+    def get_query_condition(self, parameter, value):
+        """
+        Build an ElasticSearch query object by looking up the parameter settings
+        and assigning an input value for the condition.
+        :param parameter: URL query parameter.
+        :param value: The value to look for in the index.
+        :return: Query() child object.
+        """
         settings = self.query_parameters.get(parameter)
         if not settings:
             raise KeyError("Parameter %s has no query parameters defined." % parameter)
@@ -92,40 +145,18 @@ class BaseSearch:
         query_class_parameters = {**{settings.get('query_key'): value}, **settings.get('attributes')}
         return settings.get('query_class')(**query_class_parameters)
 
+    @property
+    def supported_parameters(self):
+        """
+        Return a list of the supported query parameters of the class.
+        :return: List of Strings.
+        """
+        return list(self.query_parameters.keys())
+
 
 class CocktailSearch(BaseSearch):
     index_class = RecipeIndex
     result_class = CocktailSearchResults
-
-    def __init__(self, name, components, alpha, sort='_score'):
-        super().__init__()
-        logging.info("Searching on name=%s,components=%s,alpha=%s" % (name, components, alpha))
-        names = name.split(',')
-        components = components.split(',')
-        alphas = alpha.split(',')
-
-        musts = []
-
-        for component in components:
-            # @TODO fix this
-            if not component:
-                continue
-            musts.append(self.get_query_object(parameter='components', value=component))
-
-        for name in names:
-            if not name:
-                continue
-            musts.append(self.get_query_object(parameter='name', value=name))
-
-        # Need to do ||, not &&
-        for alpha in alphas:
-            if not alpha:
-                continue
-            musts.append(self.get_query_object(parameter='alpha', value=alpha))
-
-        print(musts)
-        self.q = Bool(must=musts)
-        self.sort = sort
 
     def _build_query_parameters(self):
         self.add_query_parameter(parameter='components', query_class=MultiMatch, query_key='query', type='phrase_prefix',
