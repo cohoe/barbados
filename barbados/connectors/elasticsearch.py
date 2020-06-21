36a1bfc226929b8df2f25f198653df8fe5a74759
Grant Cohoe <grant@grantcohoe.com>
2020-06-20 22:06:34 -0400

    clean up logging strings, add init job

--- barbados/connectors/elasticsearch.py
+++ barbados/connectors/elasticsearch.py
@@ -9,5 +9,7 @@ class ElasticsearchConnector:
         scheme = os.getenv('AMARI_ELASTICSEARCH_SCHEME', default='http')
         hosts = os.getenv('AMARI_ELASTICSEARCH_HOSTS', default=['localhost'])
         port = int(os.getenv('AMARI_ELASTICSEARCH_PORT', default=9200))
-        Log.info("Using ElasticSearch hosts: %s via %s/%i" % (hosts, scheme, port))
+
+        Log.info("Using ElasticSearch hosts: \"%s\" via %s/%i" % (hosts, scheme, port))
+
         connections.create_connection(scheme=scheme, hosts=hosts, port=port)
