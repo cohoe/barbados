d82ecdb6f5d1dcc37e11e20283a6cee249f019fa
Grant Cohoe <grant@grantcohoe.com>
2020-10-15 20:42:50 -0400

    fixed a lot of data import problems

--- barbados/objects/menuitem.py
+++ barbados/objects/menuitem.py
@@ -4,9 +4,13 @@ class MenuItem:
     in a menu. Right now this is only coded for either a "cocktail"
     or a specific Spec of a particular Cocktail.
     @TODO add a Kind field and make this generic?
+    @TODO deal with notes
     """
 
-    def __init__(self, cocktail_slug, spec_slug=None):
+    def __init__(self, cocktail_slug, spec_slug=None, notes=None):
+        if notes is None:
+            notes = []
+        self.notes = notes
         self.cocktail_slug = cocktail_slug
         self.spec_slug = spec_slug
 
