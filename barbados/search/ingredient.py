ed998637a03087bf05a4b2c2f1c29d76afb2edcd
Grant Cohoe <grant@grantcohoe.com>
2020-11-27 19:51:11 -0500

    search

--- barbados/search/ingredient.py
+++ barbados/search/ingredient.py
@@ -1,5 +1,5 @@
 from barbados.search import SearchBase
-from elasticsearch_dsl.query import MultiMatch, Match
+from elasticsearch_dsl.query import MultiMatch, Match, MatchPhrase
 from barbados.indexes import IngredientIndex
 
 
@@ -7,6 +7,12 @@ class IngredientSearch(SearchBase):
     index_class = IngredientIndex
 
     def _build_query_parameters(self):
-        self.add_query_parameter(parameter='name', query_class=MultiMatch, query_key='query', type='phrase_prefix',
+        self.add_query_parameter(url_parameter='name',
+                                 query_class=MatchPhrase,
+                                 query_key='query',
+                                 # type='phrase_prefix',
                                  fields=['slug', 'display_name', 'aliases'])
-        self.add_query_parameter(parameter='kind', query_class=Match, query_key='kind')
+        self.add_query_parameter(url_parameter='kind',
+                                 query_class=Match,
+                                 query_key='kind',
+                                 fields=['kind'])
--- barbados/search/menu.py
+++ barbados/search/menu.py
@@ -1,5 +1,5 @@
 from barbados.search import SearchBase
-from elasticsearch_dsl.query import MultiMatch, Match
+from elasticsearch_dsl.query import MultiMatch, Match, MatchPhrase
 from barbados.indexes import MenuIndex
 
 
@@ -7,5 +7,8 @@ class MenuSearch(SearchBase):
     index_class = MenuIndex
 
     def _build_query_parameters(self):
-        self.add_query_parameter(parameter='cocktail_slug', query_class=MultiMatch, query_key='query', type='phrase_prefix',
+        self.add_query_parameter(url_parameter='cocktail_slug',
+                                 query_class=MatchPhrase,
+                                 query_key='query',
+                                 # type='phrase_prefix',
                                  fields=['items.cocktail_slug'])
