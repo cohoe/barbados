74740e7ccc10dff5ab628160d3f98f04c0505368
Grant Cohoe <grant@grantcohoe.com>
2020-12-13 21:31:33 -0500

    fix ingredient name search, change resolve recipe to cocktail

--- barbados/search/ingredient.py
+++ barbados/search/ingredient.py
@@ -1,5 +1,5 @@
 from barbados.search import SearchBase
-from elasticsearch_dsl.query import MultiMatch, Match, MatchPhrase, Wildcard
+from elasticsearch_dsl.query import Match, Wildcard, MatchPhrase
 from barbados.indexes import IngredientIndex
 from barbados.search.occurrences import ShouldOccurrence
 
@@ -9,7 +9,7 @@ class IngredientSearch(SearchBase):
 
     def _build_query_parameters(self):
         self.add_query_parameter(url_parameter='name',
-                                 query_class=Wildcard,
+                                 query_class=MatchPhrase,
                                  occurrence=ShouldOccurrence,
                                  fields=['slug', 'display_name', 'aliases'])
         self.add_query_parameter(url_parameter='kind',
