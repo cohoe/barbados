c89cb84cdd62130bfb690bc7f525beeb3433941e
Grant Cohoe <grant@grantcohoe.com>
2021-01-17 16:52:44 -0500

    add new parameters to the resolution serializers

--- barbados/indexers/base.py
+++ barbados/indexers/base.py
@@ -1,19 +1,40 @@
 class BaseIndexer:
+    """
+    Template interface class for any Indexers.
+    """
     @classmethod
     def index(cls, obj):
+        """
+        Store the object in the index.
+        :param obj: barbados.objects.base.BarbadosObject child.
+        :return: None
+        """
         index = cls.factory.obj_to_index(obj, cls.for_index)
         index.save()
 
     @property
     def for_class(self):
+        """
+        Parameter containing the class that this indexer is to be used for.
+        :return: barbados.objects.base.BarbadosObject child class.
+        """
         raise NotImplementedError()
 
     @property
     def for_index(self):
+        """
+        Parameter containing the index class that this indexer corresponds to.
+        :return: barbados.indexes.base.BaseIndex child class.
+        """
         raise NotImplementedError()
 
     @property
     def factory(self):
+        """
+        Parameter containing the factory that is used to turn the object into an
+        index instance.
+        :return: barbados.factories.base.BaseFactory child class.
+        """
         raise NotImplementedError()
 
     @classmethod
@@ -43,5 +64,10 @@ class BaseIndexer:
 
     @classmethod
     def delete(cls, obj):
+        """
+        Delete a specific object from its index.
+        :param obj: barbados.objects.base.BarbadosObject child instance.
+        :return: None
+        """
         index_obj = cls.factory.obj_to_index(obj, cls.index)
         cls.index.delete(index_obj)
