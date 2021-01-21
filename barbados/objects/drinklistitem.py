a2d219b4100a6a98ab235674ad45451744b5eb0b
Grant Cohoe <grant@grantcohoe.com>
2021-01-20 21:30:15 -0500

    add support for highlighting items in a list. also cleanup idea

--- barbados/objects/drinklistitem.py
+++ barbados/objects/drinklistitem.py
@@ -10,12 +10,13 @@ class DrinkListItem(BaseObject):
     @TODO deal with notes
     """
 
-    def __init__(self, cocktail_slug, spec_slug=None, notes=None):
+    def __init__(self, cocktail_slug, spec_slug=None, notes=None, highlight=False):
         if notes is None:
             notes = []
         self.notes = notes
         self.cocktail_slug = cocktail_slug
         self.spec_slug = spec_slug
+        self.highlight = highlight
 
     def __repr__(self):
         return "Barbados::Objects::DrinkListItem[%s]" % self.cocktail_slug
@@ -23,3 +24,4 @@ class DrinkListItem(BaseObject):
     def serialize(self, serializer):
         serializer.add_property('cocktail_slug', self.cocktail_slug)
         serializer.add_property('spec_slug', self.spec_slug)
+        serializer.add_property('highlight', self.highlight)
