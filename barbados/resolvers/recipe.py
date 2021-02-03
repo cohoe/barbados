760eac30d745007406f95f70d4c27c1d7a532269
Grant Cohoe <grant@grantcohoe.com>
2021-02-02 19:07:20 -0500

    fix resolution

--- barbados/resolvers/recipe.py
+++ barbados/resolvers/recipe.py
@@ -9,17 +9,28 @@ from barbados.indexers.inventoryspec import InventorySpecResolutionIndexer
 class RecipeResolver(BaseResolver):
     @classmethod
     def resolve(cls, inventory, cocktail, spec_slug=None):
+        """
+        Process a Recipe resolution request. This request could be for all specs of a cocktail
+        or just one.
+        :param inventory: Inventory to resolve against.
+        :param cocktail: Cocktail object to resolve.
+        :param spec_slug: Optional Slug of the spec to resolve (None means do all of them)
+        :return: List of SpecResolutionSummary objects.
+        """
         results = []
 
+        # We retrieve the tree here and expand the inventory so that there isn't potential
+        # inconsistency between retrieving the tree now vs later. It does mean we have to
+        # pass it around to various functions.
         tree = IngredientTreeCache.retrieve()
         inventory.expand(tree=tree)
 
-        LogService.info("Cocktail specs: %s" % [spec.slug for spec in cocktail.specs])
+        LogService.info("Found cocktail specs: %s" % [spec.slug for spec in cocktail.specs])
         for spec in cocktail.specs:
             # Skip any specs that the user didn't ask for with the spec_slug
             # parameter.
             if spec_slug and spec.slug != spec_slug:
-                LogService.info("Skipping slug %s because you didn't want it." % spec.slug)
+                LogService.info("Skipping spec %s because you didn't want it." % spec.slug)
                 continue
 
             # Parse the spec
@@ -30,62 +41,78 @@ class RecipeResolver(BaseResolver):
 
     @staticmethod
     def _resolve_spec(inventory, cocktail, spec, tree):
+        """
+        Generate a SpecResolutionSummary object for this particular recipe. Reminder
+        that Recipe = Cocktail + Spec.
+        :param inventory: The Inventory object to resolve against.
+        :param cocktail: The Cocktail object to resolve.
+        :param spec: The Spec object that you wanted to resolve.
+        :param tree: IngredientTree.
+        :return: A SpecResolutionSummary.
+        """
         # Components use the ingredient slug as their slug so we can safely
         # assume a 1:1 mapping between them.
+        LogService.info("Resolving spec %s" % spec.slug)
         rs = SpecResolutionSummary(inventory_id=inventory.id, cocktail=cocktail, spec=spec)
 
         for component in spec.components:
-            r = None
             if inventory.contains(component.slug):
-                # The spec.component.slug is specifically named in the inventory items.
-                substitutes = inventory.substitutes(component.slug)
-                r = Resolution(slug=component.slug, status=DirectResolution, substitutes=substitutes)
-            # @TODO this block is preventing madness in the implies/substitutes
-            # @TODO I think this can stay dropped. Check out some suggestions first.
-            # elif inventory.contains(component.slug, implicit=True):
-            #     # The spec.component.slug is specifically named in the inventory implicit items.
-            #     # Targets "generic" spec components when there are "specific" or "generic"
-            #     # implicit items in the inventory.
-            #     substitutes = inventory.substitutes(component.slug, implicit=True)
-            #     print("HAHA", substitutes) if 'gin' in component.slug else None
-            #     r = Resolution(slug=component.slug, status=ImplicitResolution, substitutes=substitutes)
+                substitutes, resolution_status = RecipeResolver._get_direct_resolution(inventory, component)
             else:
-                # Now we're looking at the implied versions of the component. By definition
-                # all of these will be ImpliedResolution because we've had to change what
-                # we're looking up as.
-                substitutes = []
-                for implied_component in tree.implies(component.slug):
-                    # Test for if the inventory contains the implied component
-                    # explicitly or implicitly.
-                    if inventory.contains(implied_component):
-                        # The inventory explicitly contains this implied component.
-                        substitutes += inventory.substitutes(implied_component)
-                    elif inventory.contains(implied_component, implicit=True):
-                        # The inventory implicitly contains this implied component.
-                        substitutes += inventory.substitutes(implied_component, implicit=True)
-                    else:
-                        # The inventory does not contain this implied component in any way.
-                        continue
+                substitutes, resolution_status = RecipeResolver._get_nondirect_resolution(inventory, component, tree)
 
-                # We have to de-duplicate the substitutes we just calculated.
-                substitutes = list(set(substitutes))
-                r = Resolution(slug=component.slug, status=ImplicitResolution, substitutes=substitutes)
-
-                # By this point we haven't found any explicit or implicit matches
-                # for either the direct component or any of its implications.
-                # It's missing.
-                if r is None:
-                    r = Resolution(slug=component.slug, status=MissingResolution)
-
-            # Fill in any blanks of the component that we want to carry along
-            # in the Resolution.
-            r.parents = tree.parents(r.slug)
+            # Construct the SpecResolution object.
+            LogService.info("Resolution for %s::%s::%s is %s" % (cocktail.slug, spec.slug, component.slug, resolution_status.status))
+            r = Resolution(slug=component.slug, status=resolution_status, substitutes=substitutes,
+                           parents=tree.parents(component.slug))
 
             # Add the resolution to the summary
             rs.add_component(r)
 
-        # Index
-        # indexer_factory.get_indexer(rs).index(rs)
+        # Index and Return
         InventorySpecResolutionIndexer.index(rs)
-
         return rs
+
+    @staticmethod
+    def _get_direct_resolution(inventory, component):
+        """
+        The spec.component.slug is specifically named in the inventory items.
+        :return: Tuple of List of substitutes and DirectResolution
+        """
+        return inventory.substitutes(component.slug), DirectResolution
+
+    @staticmethod
+    def _get_nondirect_resolution(inventory, component, tree):
+        """
+        Now we're looking at the implied versions of the component. By definition
+        all of these will be ImpliedResolution because we've had to change what
+        we're looking up as.
+        This is called Nondirect because it potentially returns Missing as well.
+        :param inventory:
+        :param component:
+        :return: Tuple of List of substitutes and DirectResolution
+        """
+        substitutes = []
+        for implied_component in tree.implies(component.slug):
+            # Test for if the inventory contains the implied component
+            # explicitly or implicitly.
+            if inventory.contains(implied_component):
+                # The inventory explicitly contains this implied component.
+                substitutes += inventory.substitutes(implied_component)
+            elif inventory.contains(implied_component, implicit=True):
+                # The inventory implicitly contains this implied component.
+                substitutes += inventory.substitutes(implied_component, implicit=True)
+            else:
+                # The inventory does not contain this implied component in any way.
+                continue
+
+        # We have to de-duplicate the substitutes we just calculated.
+        substitutes = list(set(substitutes))
+
+        # By this point if we haven't found any explicit or implicit matches
+        # for either the direct component or any of its implications, it's missing.
+        # If we have then it's considered implicitly available.
+        if substitutes:
+            return substitutes, ImplicitResolution
+
+        return [], MissingResolution
